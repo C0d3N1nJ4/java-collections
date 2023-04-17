318878023141f3f76716250fa37db1caf6a9db37
naiomi.naidoo <naiomi.naidoo@capgemini.com>
2023-04-17 10:17:01 +0200

    Tutorial

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,9 +1,23 @@
 package com.linkedin.collections;
 
+import java.util.*;
+
 public class Application {
 
 	public static void main(String[] args) {
-		
+
+		Collection<String> c = new ArrayList<>();
+		c.add("One");
+		c.add("Two");
+		c.add("Three");
+		c.add("Two");
+		Collection<String> things = new HashSet<>(c);
+
+		System.out.println(c);
+		System.out.println(things);
+
+
+
 		
 	}
 
